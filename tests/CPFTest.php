33f69b04df41c0ec634790f7cbed7a3821589cae
paliari <paliari@iMac-de-paliari.local>
2013-10-18 09:42:39 -0300

    testa CPF::validarFormato

--- tests/CPFTest.php
+++ tests/CPFTest.php
@@ -9,4 +9,10 @@ class CPFTest extends PHPUnit_Framework_TestCase
         $digitos = CPF::digitos("123.456.789-01");
         $this->assertEquals("12345678901", $digitos);
     }
+
+    public function testFormato()
+    {
+        $this->assertTrue(CPF::validarFormato("123.456.789-01"));
+        $this->assertFalse(CPF::validarFormato("123.456.79-01"));
+    }
 }
