ed7baa7ced56df4626e3efb21bb6869d974726c9
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-18 10:31:10 -0300

    Adiciona comentario nos testes

--- tests/CPFTest.php
+++ tests/CPFTest.php
@@ -4,13 +4,19 @@ use Sinergia\Brasil\CPF;
 
 class CPFTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Testa o retorno do CPF somente com dígitos.
+     */
     public function testDigitos()
     {
         $digitos = CPF::digitos("123.456.789-01");
         $this->assertEquals("12345678901", $digitos);
     }
 
-    public function testFormato()
+    /**
+     * Testa se o CPF está no formato correto.
+     */
+    public function testValidarFormato()
     {
         $this->assertTrue(CPF::validarFormato("123.456.789-01"));
         $this->assertFalse(CPF::validarFormato("123.456.79-01"));
