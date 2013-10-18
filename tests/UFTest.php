4acff553a84af639a3bbe3bba2b92fc1bec24386
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-18 16:33:46 -0300

    Adiciona teste que verifica se a quantidade de estados e regioes estão corretas nas variáveis

--- tests/UFTest.php
+++ tests/UFTest.php
@@ -1,9 +1,44 @@
 <?php
 
+use Sinergia\Brasil\UF;
+
 class UFTest extends PHPUnit_Framework_TestCase
 {
-    public function test_array()
+    /**
+     * Testa se o array tem realmente 27 posições;
+     */
+    public function testCountSiglas()
     {
-      //  $this->markTestIncomplete("to do");
+        $this->assertCount(27, UF::$SIGLAS);
+    }
+
+    /**
+     * Testa se o array tem realmente 27 estados;
+     */
+    public function testCountNomes()
+    {
+        $this->assertCount(27, UF::$NOMES);
+    }
+
+    /**
+     * Testa se o array tem realmente 5 regioes;
+     */
+    public function testCountRegioes()
+    {
+        $this->assertCount(5, UF::$REGIOES);
+    }
+
+    /**
+     * Testa se todas as regioes contem 27 posições
+     */
+    public function testCountUfRegioes()
+    {
+        $i = 0;
+        foreach (UF::$REGIOES as $k => $v) {
+            foreach (UF::$REGIOES[$k] as $val) {
+                $i++;
+            }
+        }
+        $this->assertEquals(27, $i);
     }
 }
