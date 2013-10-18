fe9a36719e301228bb2ae9b9af271d57aeb00036
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-18 15:30:28 -0300

    Adiciona gerador de CPF e acrescenta validação para dígitos todos iguais Ex: 000.000.000-00 na função validar

--- Sinergia/Brasil/CPF.php
+++ Sinergia/Brasil/CPF.php
@@ -76,6 +76,11 @@ class CPF
             return false;
         }
 
+        $regex = "/^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/";
+        if (preg_match($regex, $cpf)) {
+            return false;
+        }
+
         // Primeiro dígito
         $soma = 0;
         for ($i = 0; $i < 9; $i++) {
@@ -92,5 +97,41 @@ class CPF
         $d2 = 11 - ($soma % 11);
         if ($d2 >= 10) $d2 = 0;
         return $d1 == $cpf[9] && $d2 == $cpf[10];
+    }
+
+    /**
+     * Função responsável por gerar um cpf válido
+     * @return string
+     */
+    public static function gerar()
+    {
+        $cpf = array();
+        for ($i = 0; $i < 9; $i++) {
+            $cpf[$i] = rand(0, 9);
+        }
+
+        // Primeiro dígito
+        $soma = 0;
+        for ($i = 0; $i < 9; $i++) {
+            $soma += ((10-$i) * $cpf[$i]);
+        }
+        $d1 = 11 - ($soma % 11);
+        if ($d1 >= 10) {
+            $d1 = 0;
+        }
+        $cpf[9] = $d1;
+
+        // Segundo Dígito
+        $soma = 0;
+        for ($i = 0; $i < 10; $i++) {
+            $soma += ((11-$i) * $cpf[$i]);
+        }
+        $d2 = 11 - ($soma % 11);
+        if ($d2 >= 10) {
+            $d2 = 0;
+        }
+        $cpf[10] = $d2;
+
+        return static::formatar(implode("", $cpf));
     }
 }
