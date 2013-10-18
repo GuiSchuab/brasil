c16df24ed621d30dbe58af750a46bc9a0433c82e
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-18 15:28:00 -0300

    Adiciona validação para CNPJ com casas todas iguais EX: 00.000.000/0000-00

--- Sinergia/Brasil/CNPJ.php
+++ Sinergia/Brasil/CNPJ.php
@@ -79,6 +79,11 @@ class CNPJ
             return false;
         }
 
+        $regex = "/^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/";
+        if (preg_match($regex, $cnpj)) {
+            return false;
+        }
+
         // Primeiro dígito
         $multiplicadores = array(5,4,3,2,9,8,7,6,5,4,3,2);
         $soma = 0;
