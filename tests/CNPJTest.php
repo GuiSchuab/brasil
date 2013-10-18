e62d6f662b7807b7683a7ba926fed4228e0155a1
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-18 11:13:54 -0300

    Adiciona teste CNPJ::digitos

--- tests/CNPJTest.php
+++ tests/CNPJTest.php
@@ -1,9 +1,17 @@
 <?php
 
+use Sinergia\Brasil\CNPJ;
+
 class CNPJTest extends PHPUnit_Framework_TestCase
 {
-    public function test_array()
+    /**
+     * Teste que retorna somente os dígitos od cnpj.
+     */
+    public function testDigitos()
     {
-      //  $this->markTestIncomplete("to do");
+        $digitos = CNPJ::digitos("92.122.313/0001-30");
+        $this->assertEquals("92122313000130", $digitos);
+        $digitos = CNPJ::digitos("92.12s2.313/0e001-30");
+        $this->assertEquals("92122313000130", $digitos);
     }
 }
