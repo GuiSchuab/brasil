ef802cb61f8627d81fda373bf9886559dc5176f6
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-18 11:42:58 -0300

    Adiciona teste CNPJ::formatar

--- tests/CNPJTest.php
+++ tests/CNPJTest.php
@@ -14,4 +14,21 @@ class CNPJTest extends PHPUnit_Framework_TestCase
         $digitos = CNPJ::digitos("92.12s2.313/0e001-30");
         $this->assertEquals("92122313000130", $digitos);
     }
+
+    /**
+     * Testa o retorno do CNPJ no formato 00.000.000/0000-00
+     * Caso o CNPJ informado tenha menos de 14 dígitos retorna vazio,
+     * caso maior retorna os 14 primeiro dígitos formatados
+     */
+    public function testFormatar()
+    {
+        $cnpjFormatado = CNPJ::formatar("9212-2313000130");
+        $this->assertEquals("92.122.313/0001-30", $cnpjFormatado);
+
+        $cnpjFormatado = CNPJ::formatar("92122313teste0001530");
+        $this->assertEquals("92.122.313/0001-53", $cnpjFormatado);
+
+        $cnpjFormatado = CNPJ::formatar("921223130");
+        $this->assertEquals("", $cnpjFormatado);
+    }
 }
