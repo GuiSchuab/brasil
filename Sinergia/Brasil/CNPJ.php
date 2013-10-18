5c4076a0e370ab9b8708d0a694e69d587692932c
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-18 11:28:50 -0300

    Adiciona função que retorna o CNPJ no formato 00.000.000/0000-00

--- Sinergia/Brasil/CNPJ.php
+++ Sinergia/Brasil/CNPJ.php
@@ -19,4 +19,26 @@ class CNPJ
     {
         return substr(preg_replace('![^\d]!', '', $cnpj), 0, 14);
     }
+
+    /**
+     * Retorna o cnpj formatado como: 92.122.313/0001-30
+     *
+     * @param $cnpj
+     * @return string
+     */
+    public static function formatar($cnpj)
+    {
+        $cpf = static::digitos($cnpj);
+        if (strlen($cnpj) != 14) {
+            return "";
+        }
+
+        $partes[] = substr($cnpj, 0, 2);
+        $partes[] = substr($cnpj, 3, 3);
+        $partes[] = substr($cnpj, 6, 3);
+        $filiais  = substr($cnpj, 9, 4);
+        $verificador = substr($cnpj, 13);
+
+        return implode(".", $partes) . '/' . $filiais . '-' . $verificador;
+    }
 }
