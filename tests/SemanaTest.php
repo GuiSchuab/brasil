353f2594658a87344f10495ce6ca468a1b34a651
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-21 09:05:23 -0200

    Adiciona teste para  verificar se a quantidade de siglas estão corretas

--- tests/SemanaTest.php
+++ tests/SemanaTest.php
@@ -4,5 +4,11 @@ use Sinergia\Brasil\Semana;
 
 class SemanaTest extends PHPUnit_Framework_TestCase
 {
-
+    /**
+     * Testa se o array tem realmente 7 posições;
+     */
+    public function testCountSiglas()
+    {
+        $this->assertCount(7, Semana::$SIGLAS);
+    }
 }
