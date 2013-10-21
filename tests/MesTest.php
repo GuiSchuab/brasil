52db26fe299179576ff1778429699deadaea1ae3
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-21 09:05:07 -0200

    Adiciona teste para  verificar se a quantidade de siglas estão corretas

--- tests/MesTest.php
+++ tests/MesTest.php
@@ -4,5 +4,11 @@ use Sinergia\Brasil\Mes;
 
 class MesTest extends PHPUnit_Framework_TestCase
 {
-
+    /**
+     * Testa se o array tem realmente 12 posições;
+     */
+    public function testCountSiglas()
+    {
+        $this->assertCount(12, Mes::$SIGLAS);
+    }
 }
