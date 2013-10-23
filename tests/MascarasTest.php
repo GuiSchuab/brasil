6ebc72b8ead472e7c0cea3fdd099322ab5a7d6a2
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-23 08:52:32 -0200

    Adiciona teste para CNPJ

--- tests/MascarasTest.php
+++ tests/MascarasTest.php
@@ -4,5 +4,15 @@ use Sinergia\Brasil\Mascaras;
 
 class MascarasTest extends PHPUnit_Framework_TestCase
 {
-    
+    /**
+     * Testa o retorno do CNPJ no formato 00.000.000/0000-00
+     * Caso o CNPJ informado tenha menos de 14 dígitos retorna vazio,
+     * caso maior retorna os 14 primeiro dígitos formatados
+     */
+    public function testCNPJ()
+    {
+        $this->assertEquals("92.122.313/0001-30", Mascaras::formataCNPJ("9212-2313000130"));
+        $this->assertEquals("92.122.313/0001-53", Mascaras::formataCNPJ("92122313teste0001530"));
+        $this->assertEquals("", Mascaras::formataCNPJ("921223130"));
+    }
 }
