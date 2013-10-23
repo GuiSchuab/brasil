619fc34ecc24bea27f76509207c8bb8fc4ebc852
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-23 07:58:01 -0200

    Adiciona atalhos para formatar CPF e CNPJ

--- Sinergia/Brasil/Mascaras.php
+++ Sinergia/Brasil/Mascaras.php
@@ -2,7 +2,28 @@
 
 namespace Sinergia\Brasil;
 
+use Sinergia\Brasil\CNPJ;
+use Sinergia\Brasil\CPF;
+
 class Mascaras
 {
+    /**
+     * Retorna o CNPJ no formato 00.000.000/0000-00
+     * @param $cnpj
+     * @return string
+     */
+    public static function formataCNPJ($cnpj)
+    {
+        return CNPJ::formatar($cnpj);
+    }
 
+    /**
+     * Retorna o CPF no formato 000.000.000-00
+     * @param $cpf
+     * @return string
+     */
+    public static function formataCPF($cpf)
+    {
+        return CPF::formatar($cpf);
+    }
 }
