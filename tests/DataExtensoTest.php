7db557fdcf6949048bd78776efcb398c614ab73c
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-24 14:11:50 -0200

    Adiciona funções de teste para a classe DataExtenso.php

--- tests/DataExtensoTest.php
+++ tests/DataExtensoTest.php
@@ -2,7 +2,45 @@
 
 use Sinergia\Brasil\DateTime\DataExtenso;
 
-class DataExtensoTest
+class DataExtensoTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Verifica se a data rotorna por extenso corretamente.
+     */
+    public function testDiaMesAno()
+    {
+        $this->assertEquals('25 de outubro de 2013', DataExtenso::formatar(DataExtenso::DIA_MES_ANO, strtotime('2013-10-25 11:30:01')));
+        $this->assertEquals('25 de outubro de 2013', DataExtenso::formatar(DataExtenso::DIA_MES_ANO, strtotime('20131025113001')));
+        $this->assertEquals('25 de outubro de 2013', DataExtenso::formatar(DataExtenso::DIA_MES_ANO, strtotime('10/25/2013 11:30:01')));
+    }
 
+    /**
+     * Verifica se a data rotorna por extenso corretamente.
+     */
+    public function testSemanaDiaMesAno()
+    {
+        $this->assertEquals('sexta-feira, 25 de outubro de 2013', DataExtenso::formatar(DataExtenso::SEMANA_DIA_MES_ANO, strtotime('2013-10-25 11:30:01')));
+        $this->assertEquals('sexta-feira, 25 de outubro de 2013', DataExtenso::formatar(DataExtenso::SEMANA_DIA_MES_ANO, strtotime('20131025113001')));
+        $this->assertEquals('sexta-feira, 25 de outubro de 2013', DataExtenso::formatar(DataExtenso::SEMANA_DIA_MES_ANO, strtotime('10/25/2013 11:30:01')));
+    }
+
+    /**
+     * Verifica se a data rotorna por extenso corretamente.
+     */
+    public function testDiaMesAnoHora()
+    {
+        $this->assertEquals('25 de outubro de 2013 as 11:30:01', DataExtenso::formatar(DataExtenso::DIA_MES_ANO_HORA, strtotime('2013-10-25 11:30:01')));
+        $this->assertEquals('25 de outubro de 2013 as 11:30:01', DataExtenso::formatar(DataExtenso::DIA_MES_ANO_HORA, strtotime('20131025113001')));
+        $this->assertEquals('25 de outubro de 2013 as 11:30:01', DataExtenso::formatar(DataExtenso::DIA_MES_ANO_HORA, strtotime('10/25/2013 11:30:01')));
+    }
+
+    /**
+     * Verifica se a data rotorna por extenso corretamente.
+     */
+    public function testSemanaDiaMesAnoHora()
+    {
+        $this->assertEquals('sexta-feira, 25 de outubro de 2013 as 11:30:01', DataExtenso::formatar(DataExtenso::SEMANA_DIA_MES_ANO_HORA, strtotime('2013-10-25 11:30:01')));
+        $this->assertEquals('sexta-feira, 25 de outubro de 2013 as 11:30:01', DataExtenso::formatar(DataExtenso::SEMANA_DIA_MES_ANO_HORA, strtotime('20131025113001')));
+        $this->assertEquals('sexta-feira, 25 de outubro de 2013 as 11:30:01', DataExtenso::formatar(DataExtenso::SEMANA_DIA_MES_ANO_HORA, strtotime('10/25/2013 11:30:01')));
+    }
 }
