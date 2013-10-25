987760f0d676e4db738656d45a351b1dd72d4778
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-25 08:21:17 -0200

    Adiciona função dateTimeToXMLValue

--- Sinergia/Brasil/DateTime/DateTime.php
+++ Sinergia/Brasil/DateTime/DateTime.php
@@ -4,5 +4,15 @@ namespace Sinergia\Brasil\DateTime;
 
 class DateTime
 {
-
+    /**
+     * Converte um timestamp para determinado valor para formato do XML padrao ABRASF
+     *
+     * @param timestamp $value
+     *
+     * @return string
+     */
+    public static function dateTimeToXMLValue($value)
+    {
+        return date('Y-m-d\TH:i:s', $value);
+    }
 }
