ba92f52dbc1b1a4bfafc5067d37d43b2077b9a36
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-28 13:45:14 -0200

    Move a função para o topo da classe

--- Sinergia/Brasil/DateTime/DateBr.php
+++ Sinergia/Brasil/DateTime/DateBr.php
@@ -19,6 +19,11 @@ class DateBr extends Carbon
         return parent::__construct($time, $tz);
     }
 
+    /**
+     * Utilizado pelo construtor da classe
+     * @param $date
+     * @return int
+     */
     protected function strBrToTimestamp($date)
     {
         if (preg_match('/^(0[1-9]|[1-2][0-9]|3[0-1])\/([0-1][0-2])\/(\d{4})(T| ){0,1}(([0-1][0-9]|[2][0-3]):([0-5][0-9]):([0-5][0-9])){0,1}$/', $date, $datebit)) {
@@ -31,6 +36,17 @@ class DateBr extends Carbon
             return mktime($hora, $min, $seg, $mes, $dia, $ano);
         }
         return $date;
+    }
+
+    /**
+     * Ao fazer um 'echo' na data, a função permite imprimir como se fosse uma string
+     * @return string
+     */
+    public function __toString()
+    {
+        $format = ($this->hour || $this->minute || $this->second) ? 'd/m/Y H:i:s' : 'd/m/Y';
+
+        return $this->format($format);
     }
 
     /**
@@ -80,16 +96,5 @@ class DateBr extends Carbon
         }
 
         return date($format, $date);
-    }
-
-    /**
-     * Ao fazer um 'echo' na data, a função permite imprimir como se fosse uma string
-     * @return string
-     */
-    public function __toString()
-    {
-        $format = ($this->hour || $this->minute || $this->second) ? 'd/m/Y H:i:s' : 'd/m/Y';
-
-        return $this->format($format);
     }
 }
