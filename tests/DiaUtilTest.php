b2a5280fe3e2f55fb1edd28a45c2496e6c6278cb
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-28 16:10:43 -0200

    Adiciona classe de teste para DiaUtil

---
+++ tests/DiaUtilTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Sinergia\Brasil\DateTime\DiaUtil;
+use Sinergia\Brasil\DateTime\DateBr;
+
+class DiaUtilTest extends PHPUnit_Framework_TestCase
+{
+    public function testPascoa()
+    {
+
+    }
+
+} 
