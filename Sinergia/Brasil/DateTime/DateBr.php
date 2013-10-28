69397024016266c655d44a564ab7e37ca8a982f5
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-28 09:50:49 -0200

    Adiciona função __toString para facilitar impressao usando o echo

--- Sinergia/Brasil/DateTime/DateBr.php
+++ Sinergia/Brasil/DateTime/DateBr.php
@@ -39,13 +39,26 @@ class DateBr extends Carbon
     /**
      * Converte uma timestamp para string formatada, no formato do php tradicional ex: 'd/m/Y' ou 'Y-m-d H:i:s'.
      *
-     * @param timestamp $date
+     * @param DateBr|timestamp $date
      * @param string $format
      *
      * @return string
      */
-    public static function dateTimeToString($date, $format = 'Y-m-d H:i:')
+    public static function dateTimeToString($date, $format = 'Y-m-d H:i:s')
     {
+        if ($date instanceof DateBr) {
+            $date = strtotime($date);
+        }
         return date($format, $date);
+    }
+
+    /**
+     * Ao fazer um 'echo' na data, a função permite imprimir como se fosse uma string
+     * @return string
+     */
+    public function __toString()
+    {
+        $format = ($this->hour || $this->minute || $this->second) ? 'd/m/Y H:i:s' : 'd/m/Y';
+        return $this->format($format);
     }
 }
